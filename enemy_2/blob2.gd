e8ade3fd2edcecee99b5a0e66234ccb90b845577
Héctor Mendoza <hectormjac@gmail.com>
2023-11-01 15:02:46 -0400

    Add a prototype for a hide mechanic
    There are some small bugs related to the animation but this being a prototype I wouldn't worry about it.

---
+++ enemy_2/blob2.gd
@@ -0,0 +1,154 @@
+extends CharacterBody2D
+
+signal player_hit
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+const IDLE = 0
+const JUMPING = 1
+const ON_AIR = 2
+const FALLING = 3
+const LANDING = 4
+const STUNNED = 5
+const LEFT = -1
+const RIGHT = 1
+const PLAYER_DAMAGE = 1
+
+var state = IDLE
+var direction = LEFT 
+var hp = 5
+var player_position
+var player_in_view = false
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	player_position = get_node("/root/PlayerPosition")
+
+
+func _physics_process(delta):	
+	if !is_on_floor():
+		handle_on_air(delta)
+	else:
+		handle_on_floor(delta)
+	
+	handle_animation()
+	move_and_slide()
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		player_hit.emit(PLAYER_DAMAGE)
+
+func _on_animation_timer_timeout():
+	if state == LANDING: 
+		state = IDLE
+		$AnimationTimer.wait_time = 1	
+	elif state == IDLE:
+		if player_in_view:
+			state = JUMPING
+			$AnimationTimer.wait_time = 0.5
+		else:
+			state = IDLE
+	elif state == JUMPING:
+		$AnimationTimer.wait_time = 0.5
+		hop()
+	elif state == STUNNED:
+		state = IDLE
+	
+	$AnimationTimer.start()
+	
+
+func handle_on_air(delta):
+	velocity.y += gravity * delta
+	if velocity.y > 0:
+		state = FALLING
+		$AnimatedSprite2D.animation = "on_air"
+	
+	if velocity.y < 1:
+		state = ON_AIR
+		$AnimatedSprite2D.animation = "jumping"
+	
+func handle_on_floor(_delta):
+	if state == FALLING || state == STUNNED:
+		state = LANDING
+		velocity.x = 0
+		$AnimationTimer.wait_time = 1
+	
+	if !player_in_view:
+		return
+
+	print(player_in_view)
+	
+	$RayCast2D.set_target_position(to_local(player_position.value))
+	var collider = $RayCast2D.get_collider()
+		
+	if !collider or !collider.is_in_group("player"):
+		return
+	
+	print("PLayer is in line of sight")
+	
+	handle_direction()
+	handle_in_direct_view()
+
+
+func handle_animation():
+	var STATES = {
+		IDLE: "on_floor",
+		JUMPING: "jumping",
+		ON_AIR: "on_air",
+		FALLING: "falling",
+		LANDING: "landed",
+		STUNNED: "stuned"
+	}
+	
+	var animation_name = STATES[state]
+	$AnimatedSprite2D.animation = animation_name
+
+func hop():
+	velocity.x = SPEED * direction
+	velocity.y = JUMP_VELOCITY
+	
+func handle_direction():
+	if position.x > player_position.value.x:
+		direction = LEFT
+		$AnimatedSprite2D.flip_h = true
+	else:
+		direction = RIGHT
+		$AnimatedSprite2D.flip_h = false
+
+
+func _on_area_2d_area_entered(area):
+	if !area.is_in_group("projectiles"):
+		return
+	
+	hp -= 1
+	if hp <= 0:
+		$AnimatedSprite2D.visible = false
+		$CollisionShape2D.disabled = true
+		$Area2D/CollisionShape2D.disabled = true
+		queue_free()
+		return
+	
+	state = STUNNED
+	$AnimationTimer.wait_time = 0.5
+	$AnimationTimer.start()
+
+func handle_in_direct_view():
+	if !$AnimationTimer.is_stopped():
+		return
+	
+	$AnimationTimer.wait_time = 0.5
+	$AnimationTimer.start()
+
+func _on_vission_area_body_entered(body):
+	if !body.is_in_group("player"):
+		return
+	
+	player_in_view = true
+
+
+func _on_vission_area_body_exited(body):
+	player_in_view = false
+	state = IDLE
+	$AnimationTimer.stop()
